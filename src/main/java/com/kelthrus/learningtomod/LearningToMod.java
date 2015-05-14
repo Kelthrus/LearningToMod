982a3cead7bc0e8118336e8434fe2507f689e879
kelthrus <bolivia_jfg@hotmail.com>
2015-05-13 21:40:58 -0430

    ep1-3
    going slow, about loading an empty mod to minecraft.

---
+++ src/main/java/com/kelthrus/learningtomod/LearningToMod.java
@@ -0,0 +1,30 @@
+package com.kelthrus.learningtomod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="LearningToMod", name="Learning To Mod", version="1.7.10-1.0")
+public class LearningToMod
+{
+    @Mod.Instance("LearningToMod")
+    public static LearningToMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLInitializationEvent event)
+    {
+
+    }
+}
